85e061b8452221e1918621379eff2ce2b6a7acbc
Bogdan Koch <koch@echo-company.ru>
2022-10-04 00:57:32 +0500

    composer init

---
+++ src/BracketString.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace aw3r1se\BracketProblem;
+
+/**
+ * Class BracketString
+ * @package aw3r1se\BracketProblem
+ */
+class BracketString
+{
+    private string $parseable;
+
+    public function __construct(string $parseable)
+    {
+        $this->parseable = $parseable;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isValid(): bool
+    {
+        preg_match_all('#(\()|(\))#ui', $this->parseable, $striped);
+
+        var_dump($striped);
+
+        return $striped[1] === $striped[2];
+    }
+}
